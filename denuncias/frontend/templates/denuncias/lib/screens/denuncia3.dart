9e44b8f1fcbdcd9b564d23d0f95a8f37e19a7888
AntonioMoreno <antonio.cder@gmail.com>
2023-03-01 17:32:35 -0600

    avance y cambios en clases

--- denuncias/frontend/templates/denuncias/lib/screens/denuncia3.dart
+++ denuncias/frontend/templates/denuncias/lib/screens/denuncia3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class Denuncia3 extends StatelessWidget {
   const Denuncia3({Key? key}) : super(key: key);
@@ -6,11 +7,19 @@ class Denuncia3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(left: 20, right: 50),
-          child: Column(
-            children: [],
+      body: Center(
+        heightFactor: 1,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 20, right: 50),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                FormularioDenunciante()
+              ],
+            ),
           ),
         ),
       ),
