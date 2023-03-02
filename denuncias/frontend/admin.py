98d695c055edfae7bdd790b5da8fad0691468ed2
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-03-02 00:51:30 +0000

    Nuevos

--- denuncias/frontend/admin.py
+++ denuncias/frontend/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.modeloPrueba)
+
+admin.site.register(models.denuncia)
+admin.site.register(models.estatusdenuncia)
+admin.site.register(models.domicilio)
+admin.site.register(models.lugarDenuncia)
+admin.site.register(models.evidencias)
+admin.site.register(models.domicilio)
+
--- denuncias/frontend/models.py
+++ denuncias/frontend/models.py
@@ -6,7 +6,11 @@ class modeloPrueba(models.Model):
     description = models.TextField()
     def __str__(self): 
         return self.title    
-    
+
+class estatusdenuncia(models.Model):
+    nombre = models.CharField(max_length=45)
+    descripcion = models.CharField(max_length=200)
+
 
 class denuncia(models.Model):
     numSeguimiento = models.CharField(max_length=15, primary_key=True)
@@ -14,20 +18,22 @@ class denuncia(models.Model):
     apellidoPaterno = models.CharField(max_length=45)
     apellidoPaterno = models.CharField(max_length=45)
     curp = models.CharField(max_length=45)
+    estatus=models.ForeignKey(estatusdenuncia, on_delete=models.CASCADE)
+    respuesta = models.CharField(max_length=300,null=True)
 
 class lugarDenuncia(models.Model):
-    models.ForeignKey(denuncia, on_delete=models.CASCADE)
+    denuncia=models.ForeignKey(denuncia, on_delete=models.CASCADE)
     calle = models.CharField(max_length=100)
     entreCalleUno = models.CharField(max_length=100)
     entreCalleDos = models.CharField(max_length=100)
     descripcion = models.TextField(null=True)
 
 class evidencias(models.Model):
-    models.ForeignKey(denuncia, on_delete=models.CASCADE)
+    denuncia=models.ForeignKey(denuncia, on_delete=models.CASCADE)
     url = models.TextField(max_length=500)
 
 class domicilio(models.Model):
-    models.ForeignKey(denuncia, on_delete=models.CASCADE)
+    denuncia=models.ForeignKey(denuncia, on_delete=models.CASCADE)
     municipio = models.TextField(max_length=45)
     codigoPostal = models.TextField(max_length=45)
     colonia = models.TextField(max_length=45)
