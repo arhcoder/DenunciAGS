4801a51fd8631f068efb3eafcc6a0f821f8721c7
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-03-02 14:06:18 +0000

    Recomendaciones casi terminado

--- denuncias/algoritmos/urls.py
+++ denuncias/algoritmos/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import process_image
-from .views2 import chatbot_view
+from .views2 import chatbot_view,recomendaciones_ley2
 from . import views
 
 urlpatterns = [
     #Estatus Denuncias
-    path('recomendaciones/', views.recomendaciones_ley,name="recomendaciones"),
+    path('recomendaciones/', views.recomendaciones_ley2,name="recomendaciones"),
     path('clasificador/', views.clasificar_delito,name="clasificador"),
     path('chat/', views.chat_bot,name="chat"),
     path('chat/v2/', chatbot_view,name="chat2"),
--- denuncias/algoritmos/views2.py
+++ denuncias/algoritmos/views2.py
@@ -1,4 +1,23 @@
+import base64
+from django.http import JsonResponse,request
+from django.shortcuts import render
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from datetime import datetime
+import os
+import csv
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+from django.views.decorators.csrf import csrf_exempt
+from .dataSet import data
 
+import nltk
+from nltk.chat.util import Chat, reflections
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from django.shortcuts import render
 from django.http import HttpResponse
 from nltk.chat.util import Chat, reflections
@@ -17,4 +36,49 @@ def chatbot_view(request):
     response = chatbot.respond(request.POST.get('message'))
     return HttpResponse(response)
 
+
+
+
+# Función para buscar palabras clave en los artículos de la ley
+def buscar_palabras_clave(texto, palabras_clave):
+    # Tokenizar el texto
+    tokens = word_tokenize(texto.lower())
+    # Obtener las palabras clave que no son stopwords y aplicar lematización
+    stop_words = set(stopwords.words('spanish'))
+    stemmer = SnowballStemmer('spanish')
+    palabras_clave_sin_stopwords = [stemmer.stem(p) for p in palabras_clave if not p in stop_words]
+    # Encontrar las palabras clave en el texto y aplicar lematización
+    tokens_sin_stopwords = [stemmer.stem(t) for t in tokens if not t in stop_words]
+    palabras_clave_encontradas = [p for p in palabras_clave_sin_stopwords if p in tokens_sin_stopwords]
+    return palabras_clave_encontradas
+
+# Función para obtener recomendaciones de artículos de ley
+def obtener_recomendaciones(denuncia):
+    # Tokenizar la denuncia
+    tokens = word_tokenize(denuncia.lower())
+    # Obtener las palabras clave de la denuncia y aplicar lematización
+    stemmer = SnowballStemmer('spanish')
+    palabras_clave = [stemmer.stem(t) for t in tokens if t.isalpha()]
+    # Buscar las palabras clave en los artículos de la ley
+    recomendaciones = {}
+    for articulo, texto in data.items():
+        palabras_clave_encontradas = buscar_palabras_clave(texto, palabras_clave)
+        if palabras_clave_encontradas:
+            recomendaciones[articulo] = palabras_clave_encontradas
+    return recomendaciones
+
+
+@csrf_exempt
+# Ejemplo de uso del sistema de recomendaciones de leyes
+def recomendaciones_ley2(request):
+    if request.method == 'POST':
+        my_var = request.POST.get('denuncia', None)
+        if my_var:
+            print(my_var)
+            recomendaciones = obtener_recomendaciones(str(my_var))
+            return JsonResponse(recomendaciones)
+        else:
+            return JsonResponse({'error': 'invalid input.'})
+    else:
+        return JsonResponse({'error': 'Invalid request method.'})
     
