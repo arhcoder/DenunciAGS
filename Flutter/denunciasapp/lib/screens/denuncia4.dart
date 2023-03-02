0e16dd39e5c1365c5880eba68c9550deb95f9e1f
Chisrra <75094536+Chisrra@users.noreply.github.com>
2023-03-01 20:29:53 -0600

    Add Widget select_menu and a small implementation

---
+++ Flutter/denunciasapp/lib/screens/denuncia4.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
+
+class Denuncia4 extends StatelessWidget {
+  Denuncia4({Key? key}) : super(key: key);
+
+  final List<String> reportOption = [
+    "ROBO DE OBJETOS",
+    "ROBO DE VEHÍCULOS",
+    "ROBO A CASA HABITACIÓN",
+    "VIOLENCIA DOMESTICA",
+    "LESIONES CULPUSOS",
+    "LESIONES DOLOSAS",
+    "DAÑO A LA PROPIEDAD",
+    "AMENAZAS",
+    "DELITOS CONTRA LA SALUD RELACIONADOS CON NARCÓTICOS",
+    "FRAUDE",
+    "HOMICIDIO",
+    "DESPOJO",
+    "BUSO DE CONFIANZA",
+    "ABUSO SEXUAL",
+    "DELITOS CONTRA MENORES"
+  ];
+  final String selectedOption = "ROBO DE OBJETOS";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        heightFactor: 1,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 20, right: 50),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                SelectMenuWidget(
+                    options: reportOption,
+                    selectedOption: selectedOption,
+                    onChanged: (value) {
+                      print('Seleccionaste la opción $value');
+                    }
+                  )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- Flutter/denunciasapp/lib/widgets/widgets.dart
+++ Flutter/denunciasapp/lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'titlesags.dart';
 export 'subtitlesags.dart';
 export 'custom_form_widget1.dart';
 export 'formulario_de_denunciante.dart';
+export 'select_menu.dart';
