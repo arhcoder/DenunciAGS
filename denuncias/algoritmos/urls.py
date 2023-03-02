1cd3824a257190e3fb5efd654326dc41aaf56c02
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-03-02 13:44:51 +0000

    Cambios Emilio

--- denuncias/algoritmos/urls.py
+++ denuncias/algoritmos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import process_image
+from .views2 import chatbot_view
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,6 @@ urlpatterns = [
     path('recomendaciones/', views.recomendaciones_ley,name="recomendaciones"),
     path('clasificador/', views.clasificar_delito,name="clasificador"),
     path('chat/', views.chat_bot,name="chat"),
+    path('chat/v2/', chatbot_view,name="chat2"),
     path("send_ine/", views.process_image, name="process_image"),
     ]
---
+++ denuncias/algoritmos/views2.py
@@ -0,0 +1,18 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from nltk.chat.util import Chat, reflections
+
+def chatbot_view(request):
+    pairs = [
+        ['Hola|Hola!|Hola ¿qué tal?', ['Hola, ¿cómo estás?', '¡Hola!']],
+        ['¿Cómo estás?|¿Cómo te sientes?', ['Estoy bien, gracias. ¿Y tú?', 'Me siento muy bien, gracias']],
+        ['¿Cuál es tu nombre?|¿Cómo te llamas?', ['Me llamo Chatbot, ¿y tú?', 'Mi nombre es Chatbot']],
+        ['Adiós|Hasta luego|Chao|Hasta pronto', ['Adiós, ¡que tengas un buen día!', 'Hasta luego']],
+    ]
+
+    chatbot = Chat(pairs, reflections)
+    response = chatbot.respond(request.GET.get('message'))
+    return HttpResponse(response)
+
+    
